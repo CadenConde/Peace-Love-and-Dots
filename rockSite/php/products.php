9467ce873d7488337c2ed961ea040b72e99fe409
Rechubcire <114120207+Rechubcire@users.noreply.github.com>
2023-02-06 10:00:59 -0500

    pic put into a tbl

--- rockSite/php/products.php
+++ rockSite/php/products.php
@@ -1,5 +1,38 @@
-<?php include('partials/menu.php'); ?>
-    
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!--Makes website more resposive-->
+    <meta name="viewport" content="width=device-width" , initial-scale="1.0">
+    <link rel="stylesheet" href="../css/stylesheet.css">
+    <title>Peace, Love, and Dots</title>
+</head>
+
+<body>
+
+    <!--Navbar (Header) Start-->
+    <section class="nav section-color">
+        <div class="navbar ">
+            
+                <img src="../images/rockdudes_banner.png"
+                alt="Heart Rock in cornner with company motto 'peace love and dots next to it'" class="banner-style">
+            
+            <ul>
+                <li>
+                    <a href="home.php" class="navlink">Home</a>
+                </li>
+                <li>
+                    <a href="products.php" class="navlink">Products</a>
+                </li>
+                <li>
+                    <a href="about.php" class="navlink">About</a>
+                </li>
+                
+            </ul>
+            
+        </div>
+    </section>
+    <!--Navbar End-->
 
     <!--Start of Banner-->
     <section>
@@ -11,105 +44,89 @@
 
     <!--Start of Categories-->
     <section>
-        <div class="container">
-            <div class="float">
-                <img src="../images/shop_rock4.png" class="img-curve img-respond-medium " alt="Ornamant on tree">
-                <h4 class="descriptions"><small class="extra">Ornaments & <br>Key Chains</small></h4>
-            </div>
+        <table>
+            <tr>
+                <td>
+                    <img src="../images/shop_rock4.png" class="img-curve img-respond-medium " alt="Ornamant on tree">
+                    
+                </td>
 
-            <div class="float">
-                <img src="../images/shop_rock3.png" class="img-curve img-respond-medium ">
-                <h4 class="descriptions"><small>Mushrooms</small></h4>
-            </div>
+                <td>
+                    <img src="../images/shop_rock3.png" class="img-curve img-respond-medium ">
+                    
+                </td>
 
-            <div class="float">
-                <img src="../images/shop_rock2.png" class="img-curve img-respond-medium ">
-                <h4 class="descriptions"><small>Hearts</small></h4>
-            </div>
-            <div class="float">
-                <img src="../images/shop_rock1.png" class="img-curve img-respond-medium ">
-                <h4 class="descriptions"><small>Candle Holders</small></h4>
-            </div>
-        </div>
+                <td>
+                    <img src="../images/shop_rock2.png" class="img-curve img-respond-medium ">
+                    
+                </td>
+                <td>
+                    <img src="../images/shop_rock1.png" class="img-curve img-respond-medium ">
+                    
+                </td>
+            </tr>
+            <tr>
+                <th>Ornaments & Key Chains</th>
+                <th>Mushrooms</th>
+                <th>Hearts</th>
+                <th>Candle Holders</th>
+            </tr>
+        </table>
     </section>
     <br><br>
     <hr>
-    <section>
-        <!--Start in Left side Info Box-->
-        <table style="width:auto" class="ordering">
-            <div>
-                <tr>
-                    <td>
-                        <h2>Prices,<br>
-                            Details &<br>
-                            Custom Orders</h2>
-                        <br><br>
-                        <p>The etsy hyperlink below<br> will lead you to our shopping page<br> where you can order  your own
-                            custom dot art</p>
+    <br><br>
+    <!--Start of example Products-->
+<section>
+    <table>
+        <tr>
+            <td>
+                <img src="../images/shop_rock5.png" alt="Black blue and purple candle holder" class="img-respond-small img-curve">
+                <h5><small>Blue & Gray Candle<br>Holder</small></h5>
+            </td>
 
+            <td>
+                <img src="../images/shop_rock6.png" alt="black heart rock paiont with Rainbow color" class="img-respond-small img-curve">
+                <h5><small>Black Painted Heart<br>With Rainbow</small></h5>
+            </td>
+        
+            <td>
+                <img src="../images/shop_rock7.png" alt="5 rocks in a circle" class="img-respond-small img-curve">
+                <h5><small>Individually Colored<br>Rainbow Rocks</small></h5>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <img src="../images/shop_rock8.png" alt="Black Mushroom rock with Rainbow dots" class="img-respond-small img-curve">
+                <h5><small>Rainbow on<br>Mushroom Rock</small></h5>
+            </td>
 
-                        <!--Etsy Hyperlink Button-->
+            <td>
+                <img src="../images/shop_rock9.png" alt="3 black and colored dot keychains" class="img-respond-small img-curve">
+                <h5><small>Three Painted<br>Keychains</small></h5>
+            </td>
 
-                        <br>
-                        <div class="center">
-                        <a href="https://www.etsy.com/shop/peaceloveanddots/?etsrc=sdt"
-                            class="btn-hyperlink-second navbar">Etsy Shop</a>
-                        </div>
-                        <br>
-                    </td>
-                </tr>
-            </div>
-            <!--End of Left Side info Box-->
-
-            <!--Start of Product Examples-->
-            <div>
-                <tr>
-                    <div class="rock-tbl">
-                        <div>
-                            <img src="../images/shop_rock5.png" alt="Black blue and purple candle holder"
-                                class="img-respond-small img-curve">
-                            <h5><small>Blue & Gray Candle<br>Holder</small></h5>
-                        </div>
-
-                        <div>
-                            <img src="../images/shop_rock6.png" alt="black heart rock paiont with Rainbow color"
-                                class="img-respond-small img-curve">
-                            <h5><small>Black Painted Heart<br>With Rainbow</small></h5>
-                        </div>
-
-                        <div>
-                            <img src="../images/shop_rock7.png" alt="5 rocks in a circle"
-                                class="img-respond-small img-curve">
-                            <h5><small>Individually Colored<br>Rainbow Rocks</small></h5>
-                        </div>
-                    </div>
-                </tr>
-                <tr>
-                    <div class="rock-tbl">
-                        <div>
-                            <img src="../images/shop_rock8.png" alt="Black Mushroom rock with Rainbow dots"
-                                class="img-respond-small img-curve">
-                            <h5><small>Rainbow on<br>Mushroom Rock</small></h5>
-                        </div>
-
-                        <div>
-                            <img src="../images/shop_rock9.png" alt="3 black and colored dot keychains"
-                                class="img-respond-small img-curve">
-                            <h5><small>Three Painted<br>Keychains</small></h5>
-                        </div>
-
-                        <div>
-                            <img src="../images/shop_rock10.png" alt="Black candle holder rock with purple blue and White"
-                                class="img-respond-small img-curve">
-                            <h5><small>Green & Blue<br>on Painted<br>Candle Holder</small></h5>
-                        </div>
-                    </div>
-                </tr>
-            </div>
-        </table>
-
-    </section>
+            <td>
+                <img src="../images/shop_rock10.png" alt="Black candle holder rock with purple blue and White" class="img-respond-small img-curve">
+                <h5><small>Green & Blue<br>on Painted<br>Candle Holder</small></h5>
+            </td>
+        </tr>
+    </table>
+</section>
+<br><br>
     <hr>
     <!-- End of Categories -->
 
-    <?php include('partials/footer.php'); ?>
+    <!--Footer Start-->
+    <section>
+        <br><br><br><br>
+        <div class="center">
+            <img src="../images/Rock-Dudes-Logo.png" alt="Company Logo" class="img-logo img-curve">
+            <h6>Peace Love and Dots</h6>
+        </div>
+    </section>
+    <!--Footer Ends-->
+    </div>
+</body>
+
+</html>
