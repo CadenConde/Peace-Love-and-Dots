a614e59fc13e9ee5879f8f063f54e0d0d86765e6
Chad <61475148+ChadDreemurr@users.noreply.github.com>
2023-02-08 22:34:34 -0500

    continue work on orders

--- rockSite/php/order.php
+++ rockSite/php/order.php
@@ -1,2 +1,102 @@
-<?php echo "hello"?>
-<?php echo $_GET['id'];?>
+<?php include('partials/menu.php'); ?>
+<br><br><br><br><br><br>
+
+<?php 
+    $id =  $_GET['id'];
+    $sql = "SELECT * FROM products WHERE ID = '$id';";
+    $res = mysqli_query($conn, $sql);
+    if ($res == TRUE) {
+        $rows = mysqli_fetch_assoc($res);
+            $id = $rows['ID'];
+            $prod_name=$rows['Name'];
+            $prod_descr=$rows['Description'];
+            $image=$rows['imageFile'];
+            $price=number_format($rows['Price'], 2);
+            
+            ?>
+            <div class="center">
+                <h1>ORDER ITEM</h1>
+                <img src="<?php echo SITEURL;?>/images/<?php echo $image; ?>" alt="<?php echo $prod_descr; ?>" width = "20%">
+                <h3>$<?php echo $price; ?></h3>
+            </div>
+                <form action="" method="POST" enctype="multipart/form-data">
+                    <table class="rock-tbl">
+                        <tr><td><u>CUSTOMER INFO</u></td></tr>
+                        <tr>
+                            <td>First Name: </td>
+                            <td> 
+                                <input type="text" name="fName" placeholder="First Name">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Last Name: </td>
+                            <td width = "20%";> 
+                                <input type="text" name="lName" placeholder="Last Name">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Email: </td>
+                            <td> 
+                                <input type="text" name="email" placeholder="e.g. johnsmith@gmail.com">
+                            </td>
+                        </tr>
+                        <tr><td><u>SHIPPING INFO</u></td></tr>
+                        <tr>
+                            <td>Address Line 1: </td>
+                            <td> 
+                                <input type="text" name="line1" placeholder="Address Line 1">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Address Line 2: </td>
+                            <td> 
+                                <input type="text" name="line2" placeholder="(optional)">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>City</td>
+                            <td> 
+                                <input type="text" name="city" placeholder="e.g. New York">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>State</td>
+                            <td> 
+                                <input type="text" name="state" placeholder="e.g. NY">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Zip Code</td>
+                            <td> 
+                                <input type="text" name="zip" placeholder="e.g. 12345">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <input type="submit" name="submit" value="Proceed to Checkout" class="btn-secondary">
+                            </td>
+                        </tr>
+                    </table>
+                </form>         
+            <?php
+            
+    }
+    if (isset($_POST['submit'])) {
+        $fname = $_POST['fName'];
+        $lname = $_POST['lName'];
+        $email = $_POST['email'];
+        $address1 = $_POST['line1'];
+        $address2 = $_POST['line2'];
+        $address3 = $_POST['city'] . " " . $_POST['state'] . " " . $_POST['zip'];
+        $vars = "?prod_id=$id&fname=$fname&lname=$lname&email=$email&address1=$address1&address2=$address2&address3=$address3";
+
+        echo '<script type="text/javascript">function jsFunction(){location.href = "send-order.php'.$vars.'";}</script>';
+        echo '<script type="text/javascript">jsFunction();</script>'; //things broke so i cheated the link
+
+    }
+
+    
+    ?>
+
+
+<?php include('partials/footer.php');?>
--- rockSite/php/products.php
+++ rockSite/php/products.php
@@ -1,9 +1,5 @@
 <?php include('partials/menu.php'); ?>
 
-<!DOCTYPE html>
-
-<html>
-<body class="center">
     <br><br><br><br><br><br><br><br>
     <!--Start of Categories-->
     <section>
@@ -43,87 +39,46 @@
 <section>
     <table>
     <?php 
-                    $sql = "SELECT * FROM products";
-                    $res = mysqli_query($conn, $sql);
-                    if ($res==TRUE)
-                    {
-                        $count = mysqli_num_rows($res);
-                        if ($count>0)
-                        {
-                            while($rows=mysqli_fetch_assoc($res))
-                            {
-                                $id = $rows['ID'];
-                                $prod_name=$rows['Name'];
-                                $prod_descr=$rows['Description'];
-                                $image=$rows['imageFile'];
-                                $price=number_format($rows['Price'], 2);
-                                
-                                ?>
-                               
-                                <tr>
-                                    <td width = "20%"><img src="<?php echo SITEURL;?>/images/<?php echo $image; ?>" alt="<?php echo $prod_descr; ?>" width = "90%"></td>
+        $sql = "SELECT * FROM products";
+        $res = mysqli_query($conn, $sql);
+        if ($res==TRUE)
+        {
+            $count = mysqli_num_rows($res);
+            if ($count>0)
+            {
+                while($rows=mysqli_fetch_assoc($res))
+                {
+                    $id = $rows['ID'];
+                    $prod_name=$rows['Name'];
+                    $prod_descr=$rows['Description'];
+                    $image=$rows['imageFile'];
+                    $price=number_format($rows['Price'], 2);
+                    
+                    ?>
+                    
+                    <tr>
+                        <td width = "20%"><img src="<?php echo SITEURL;?>/images/<?php echo $image; ?>" alt="<?php echo $prod_descr; ?>" width = "90%"></td>
 
-                                    <td><?php echo $prod_name;?></td>
-
-
-                                    <td> $<?php echo $price;?></td>
-
-                                    <td width = '20%'>
-                                        <button onclick="orderProd(<?php echo $id;?>)" class="btn-secondary">Order</button><br>
-                                    </td>
-                                </tr>
-                                <?php
-                            }
-                        } else {
-                            echo "<tr>no products found</tr>";
-                        }
-                    }
-                ?>
-            
-            
+                        <td><?php echo $prod_name;?></td>
 
 
+                        <td> $<?php echo $price;?></td>
 
-
-
-        <!--
-            <tr>
-            <td>
-                <img src="../images/shop_rock5.png" alt="Black blue and purple candle holder" class="img-respond-small img-curve">
-                <h5><small>Blue & Gray Candle<br>Holder</small></h5>
-            </td>
-
-            <td>
-                <img src="../images/shop_rock6.png" alt="black heart rock paiont with Rainbow color" class="img-respond-small img-curve">
-                <h5><small>Black Painted Heart<br>With Rainbow</small></h5>
-            </td>
-        
-            <td>
-                <img src="../images/shop_rock7.png" alt="5 rocks in a circle" class="img-respond-small img-curve">
-                <h5><small>Individually Colored<br>Rainbow Rocks</small></h5>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <img src="../images/shop_rock8.png" alt="Black Mushroom rock with Rainbow dots" class="img-respond-small img-curve">
-                <h5><small>Rainbow on<br>Mushroom Rock</small></h5>
-            </td>
-
-            <td>
-                <img src="../images/shop_rock9.png" alt="3 black and colored dot keychains" class="img-respond-small img-curve">
-                <h5><small>Three Painted<br>Keychains</small></h5>
-            </td>
-
-            <td>
-                <img src="../images/shop_rock10.png" alt="Black candle holder rock with purple blue and White" class="img-respond-small img-curve">
-                <h5><small>Green & Blue<br>on Painted<br>Candle Holder</small></h5>
-            </td>
-        </tr>
-        -->
+                        <td width = '20%'>
+                            <button onclick="sendOrder(<?php echo $id;?>)" class="btn-secondary">Order</button><br>
+                        </td>
+                    </tr>
+                    <?php
+                }
+            } else {
+                echo "<tr>no products found</tr>";
+            }
+        }
+    ?>
     </table>
 </section>
         <script>
-            function orderProd(id=0) {
+            function sendOrder(id=0) {
                 location.href = "order.php?id=" + id;
             }
         </script>
@@ -137,4 +92,4 @@
 
 </html>
 
-<?php include(partials/footer.php)?>
+<?php include('partials/footer.php'); ?>
