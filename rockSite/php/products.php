de58107dff96c96137e821828169af660810a472
Chad <61475148+ChadDreemurr@users.noreply.github.com>
2023-02-14 09:31:13 -0500

    Added sorting

--- rockSite/php/products.php
+++ rockSite/php/products.php
@@ -1,8 +1,44 @@
 <?php include('partials/menu.php'); ?>
 
-    <br><br><br><br><br><br><br><br>
+    <br><br><br>
     <!--Start of Categories-->
     <section>
+        <div class="center">
+            <h2>Shop</h2>
+        </div>
+        <div class="sortBy" align="right">
+            <div class="dropdown"><u>Sort By Categories</u>
+                <div class="dropdown-content">
+                    <?php   
+                    $sql = "SELECT * FROM prod_types";
+                    $res = mysqli_query($conn, $sql);
+                    if ($res==TRUE)
+                    {
+                        $count = mysqli_num_rows($res);
+                        if ($count>0)
+                        {
+                            while($rows=mysqli_fetch_assoc($res))
+                            {
+                                $id = $rows['id'];
+                                $type = $rows['type'];
+                    
+                                
+                                ?>
+                                    <a href="products.php?sort=<?php echo $type;?>"><?php echo $type; ?></a>
+                                <?php
+                                
+                            }
+                        }
+                    }
+                    ?>
+                    <a href="products.php">All</a>
+
+                </div>
+            </div>
+            
+            
+        </div>
+        <!--
         <table class="rock-tbl">
             <tr>
                 <td>
@@ -31,9 +67,9 @@
                 <th>Candle Holders</th>
             </tr>
         </table>
+        -->
     </section>
-    <br><br>
-    <hr>
+    
     <br><br>
     <!--Start of example Products-->
 <section>
@@ -46,6 +82,7 @@
             $count = mysqli_num_rows($res);
             if ($count>0)
             {
+                $counter = 0;
                 while($rows=mysqli_fetch_assoc($res))
                 {
                     $id = $rows['ID'];
@@ -53,26 +90,54 @@
                     $prod_descr=$rows['Description'];
                     $image=$rows['imageFile'];
                     $price=number_format($rows['Price'], 2);
+                    if(isset($_GET['sort'])){
+                        $sorter = $_GET['sort'];
+                        if($prod_descr == $sorter){
+                            $counter++;
+                        ?>
                     
-                    ?>
+                        <tr>
+                            <td width = "20%"><img src="<?php echo SITEURL;?>/images/<?php echo $image; ?>" alt="<?php echo $prod_descr; ?>" width = "90%"></td>
+    
+                            <td><?php echo $prod_name;?></td>
+    
+    
+                            <td> $<?php echo $price;?></td>
+    
+                            <td width = '20%'>
+                                <button onclick="sendOrder(<?php echo $id;?>)" class="btn-secondary">Order</button><br>
+                            </td>
+                        </tr>
+                        <?php
+                        }
+                    }
+                    else{
+                        $counter++;
+                        ?>
                     
-                    <tr>
-                        <td width = "20%"><img src="<?php echo SITEURL;?>/images/<?php echo $image; ?>" alt="<?php echo $prod_descr; ?>" width = "90%"></td>
-
-                        <td><?php echo $prod_name;?></td>
-
-
-                        <td> $<?php echo $price;?></td>
-
-                        <td width = '20%'>
-                            <button onclick="sendOrder(<?php echo $id;?>)" class="btn-secondary">Order</button><br>
-                        </td>
-                    </tr>
-                    <?php
+                        <tr>
+                            <td width = "20%"><img src="<?php echo SITEURL;?>/images/<?php echo $image; ?>" alt="<?php echo $prod_descr; ?>" width = "90%"></td>
+    
+                            <td><?php echo $prod_name;?></td>
+    
+    
+                            <td> $<?php echo $price;?></td>
+    
+                            <td width = '20%'>
+                                <button onclick="sendOrder(<?php echo $id;?>)" class="btn-secondary">Order</button><br>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                    
+                }
+                if($counter == 0){
+                    echo '<div class = "center">No Products Found</div>';
                 }
             } else {
-                echo "<tr>no products found</tr>";
+                echo '<div class = "center">No Products Found</div>';
             }
+            
         }
     ?>
     </table>
