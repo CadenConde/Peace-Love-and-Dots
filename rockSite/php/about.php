6b1acf625fb560e8a0725baa08fb299ce8ad7592
Chad <61475148+ChadDreemurr@users.noreply.github.com>
2023-02-22 09:44:00 -0500

    get about from database

--- rockSite/php/about.php
+++ rockSite/php/about.php
@@ -3,33 +3,25 @@
     <!--About Info Starts-->
    <section>
        
-            <br><br>
-            <h2 style="text-align:center">About Me!</h2>
-   
+        <br><br>
+        <h2 style="text-align:center">About Me!</h2>
+        <?php 
+               $sql = "SELECT * FROM about"; //get about from database
+               $res = mysqli_query($conn, $sql);
+               if ($res == TRUE) {
+                   $row=mysqli_fetch_assoc($res);
+                   $count = mysqli_num_rows($res);
+                   if ($count > 0) {
+                       $about = $row['about'];
+                   }
+               }
+               ?>
         <div>
         <img src="../images/PPRock Dudes.png" alt="Owner's Profile Picture" class="img-respond" style="float: left;">
         </div>
         <div class="about-text">
-            <p> Rock and mandala painting with love and hope</p>
-            <br>
-            <p> When the world fell apart in 2020, I began painting rocks and mandalas as stress relief and as a family activity with my daughters. It quickly became my favorite pastime (obsession??) when I have a few minutes - or hours!</p>
-            <br>    
-            <p> I love experimenting with shapes of stones, color combinations, and different tools, and have discovered different designs and colors “speak” to me at different times. The most important lesson I’ve learned from painting is to focus on intention, not expectation, and let the energy come. I believe nature and color are powerful forces in the world, and I want to share those forces through my art.</p>
-            <br>    
-            <p>Peace, love & blessings.</p>
+            <?php echo $about?>
         </div>
         <!--About Info Ends-->
-
-        <!-- Social Links Start-->
-
-        <section>
-            <div class="center">
-                <h3 style="font-weight:lighter;">Social Media</h3>
-            </div>
-            <br><br>
-            <div class="center">
-
-            </div>
-        </section>
 <hr>
    <?php include('partials/footer.php'); ?>
