99daa856c611523d7e100f3557c8383a65a2dc14
sunsettdevv <sunsettdevv@gmail.com>
2023-03-02 16:28:59 -0500

    Updated footer to comply with ADA

--- rockSite/php/partials/footer.php
+++ rockSite/php/partials/footer.php
@@ -1,13 +1,10 @@
+<!-- Page footer -->
 <section>
-    
     <div class="center">
         <img src="../images/Rock-Dudes-Logo.png" alt="Company Logo" class="img-respond-small">
-        <p style="font-size:13px">Want to be updated when new products are posted? Click <a style="font-size:13px" href="join-maillist.php">here</a> to sign up for updates and newsletters! <br> Any Questions? Contact us at <?php echo Client_Email;?></p>
-        
-        
+        <p style="font-size:13px">Want to be updated when new products are posted? <a style="font-size:13px" href="join-maillist.php">Sign up</a> for updates and newsletters! <br> Any Questions? Contact us at <?php echo Client_Email;?></p>
     </div>
 </section>
     <br>
-    <!--Footer Ends-->
     </body>
 </html>
