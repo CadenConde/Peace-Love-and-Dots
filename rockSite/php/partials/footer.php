4d321848908448a6d8157b95fd0b7e080cccac1a
Chad <61475148+ChadDreemurr@users.noreply.github.com>
2023-03-01 23:54:36 -0500

    edit footer

--- rockSite/php/partials/footer.php
+++ rockSite/php/partials/footer.php
@@ -2,7 +2,8 @@
     
     <div class="center">
         <img src="../images/Rock-Dudes-Logo.png" alt="Company Logo" class="img-respond-small">
-        <p style="font-size:13px">Want to be updated when new products are posted? Click <a style="font-size:13px" href="join-maillist.php">here</a> to sign up for updates and newsletters!</p>
+        <p style="font-size:13px">Want to be updated when new products are posted? Click <a style="font-size:13px" href="join-maillist.php">here</a>to sign up for updates and newsletters! <br> Any Questions? Contact us at <?php echo Client_Email;?></p>
+        
         
     </div>
 </section>
