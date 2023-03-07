0ea8d0227b881803a92707474eca06d02eb78b5e
Chad <61475148+ChadDreemurr@users.noreply.github.com>
2023-03-07 10:02:42 -0500

    various styling changes, mostly carousel

--- rockSite/php/home.php
+++ rockSite/php/home.php
@@ -4,7 +4,11 @@
     <script src="../scripts/scroll.js" defer></script>
     <link href="../css/flickity.css" media="screen" rel="stylesheet">
     <script src="../scripts/flickity.pkgd.min.js"></script>
-    
+    <?php
+        $sql = "SELECT * FROM products WHERE featured = '1' && sold = '0';";
+        $res = mysqli_query($conn, $sql);
+        $count = mysqli_num_rows($res);
+    ?>
     <div class="center">
         <br><br><br><br><br>
         <!-- Featured products Start -->
@@ -17,7 +21,9 @@
                 <img src="../images/banner_heart_right.png" width="15%;" alt="Heart on right side of banner">
             </div>
 
-            <div class="gallery js-flickity" data-flickity-options='{ "wrapAround": true, "autoPlay": true, "lazyLoad": true }'>
+            <!--adjust width based on number of items-->
+            <div class="gallery js-flickity" style="width:<?php $width = (15*$count); if($width<=70){echo $width;}else{echo "70";};?>%"
+            data-flickity-options='{"pauseAutoPlayOnHover": false, "wrapAround": true, "autoPlay": 3000, "lazyLoad": true, "prevNextButtons": false, "pageDots": false}'>
 
                 <?php 
                     $sql = "SELECT * FROM products WHERE featured = '1' && sold = '0';";
@@ -39,6 +45,23 @@
                                 <div class="gallery-cell"><a href="order.php?id=<?php echo $id?>"><img src="<?php echo SITEURL;?>/images/<?php echo $image; ?>" alt="<?php echo $prod_descr; ?>" class="carousel-img"></a></div>
                                 <?php
                             }
+
+                            $sql = "SELECT * FROM products WHERE featured = '1' && sold = '0';"; //do it twice for cleaner carousel loop
+                            $res = mysqli_query($conn, $sql);
+                            $count = mysqli_num_rows($res);
+                            $counter = 0;
+                            while($rows=mysqli_fetch_assoc($res))
+                            {
+                                $id = $rows['ID'];
+                                $prod_name=$rows['Name'];
+                                $prod_descr=$rows['Description'];
+                                $image=$rows['imageFile'];
+                                $price=number_format($rows['Price'], 2);
+                                ?>
+                                <div class="gallery-cell"><a href="order.php?id=<?php echo $id?>"><img src="<?php echo SITEURL;?>/images/<?php echo $image; ?>" alt="<?php echo $prod_descr; ?>" class="carousel-img"></a></div>
+                                <?php
+                            }
+
                         } else {
                             echo "no featured items found";
                         }
